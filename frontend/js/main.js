c8eb2fc3cc438365f09db1578e2f554f4f774ab7
DhaanuI <dhaanu97@gmail.com>
2023-07-22 11:38:41 +0530

    minor css

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,16 +3,17 @@ const chatMessages = document.querySelector(".chat-messages")
 const roomName = document.getElementById("room-name")
 const userList = document.getElementById("users")
 
-const urlParams = new URLSearchParams(window.location.search)
+const urlParams = new URLSearchParams(window.location.search)  // to get room name and username from form
 
 const username = urlParams.get("username")
 const room = urlParams.get("room")
 
-console.log(username, room)
+//console.log(username, room)
 
-const socket = io("http://localhost:8080/", { transports: ["websocket"] });
+const socket = io("https://videochat-nxaa.onrender.com/", { transports: ["websocket"] });
 
 socket.emit("joinRoom", { username, room });
+
 
 socket.on("message", (message) => {
     outputMessage(message)
@@ -20,13 +21,14 @@ socket.on("message", (message) => {
     chatMessages.scrollTop = chatMessages.scrollHeight;
 })
 
+
 socket.on("roomUsers", ({ room, users }) => {
     outputRoomName(room)
     outputRoomUsers(users)
 })
 
-// get message and send
 
+// send message
 chatForm.addEventListener("submit", (e) => {
     e.preventDefault()
 
@@ -45,7 +47,7 @@ chatForm.addEventListener("submit", (e) => {
 })
 
 
-// outPut message 
+// 
 function outputMessage(message) {
     const div = document.createElement("div");
     div.classList.add("message");
@@ -69,11 +71,13 @@ function outputMessage(message) {
     chatMessages.appendChild(div);
 }
 
-
+//get room name
 function outputRoomName(room) {
     roomName.innerText = room
 }
 
+
+// get room users
 function outputRoomUsers(users) {
     userList.innerHTML = ''
     users.forEach(user => {
@@ -85,7 +89,7 @@ function outputRoomUsers(users) {
 
 
 document.getElementById("leave-btn").addEventListener("click", (e) => {
-    const leaveRoom = confirm("Are you sure you want to leave the chatRoom?");
+    const leaveRoom = confirm("Are you sure you want to leave the Room?");
 
     if (leaveRoom) {
         window.location.href = "../index.html";
@@ -95,105 +99,104 @@ document.getElementById("leave-btn").addEventListener("click", (e) => {
 
 
 
-/// Video
+// Video
+const videoGrid = document.getElementById('video-grid')
 
-    const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer();  // to establish peer to peer
 
-    const myPeer = new Peer();
+const myVideo = document.createElement('video')
+myVideo.muted = true   // mute our own audio so that we wont be listening to our own speech
 
-    const myVideo = document.createElement('video')
-    myVideo.muted = true   // mute our own audio so that we wont be listening to our own speech
-
-    const peers = {}
-
+const peers = {}
 
 
 
-    navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true
-    }).then(stream => {
-        addVideoStream(myVideo, stream)   // render video
 
-        // listens or receives other user video to render on our screen
-        myPeer.on('call', call => {
-            call.answer(stream)                             // it is like answering video call
-            const video = document.createElement('video')
-            call.on('stream', userVideoStream => {
-                addVideoStream(video, userVideoStream)      // and send back video to them
-            })
-        })
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)   // render video
 
-        // sends video to other user
-        socket.on('user-connected', userId => {
-            connectToNewUser(userId, stream)
-        })
-
-        const toggleVideoButton = document.getElementById('toggle-video-button');
-        toggleVideoButton.addEventListener('click', toggleVideoStream);
-
-        function toggleVideoStream() {
-            const videoTrack = stream.getVideoTracks()[0];
-
-            if (videoTrack.enabled) {
-                videoTrack.enabled = false;
-                myVideo.srcObject.getVideoTracks()[0].enabled = false;
-                toggleVideoButton.textContent = 'Start Video';
-            } else {
-                videoTrack.enabled = true;
-                myVideo.srcObject.getVideoTracks()[0].enabled = true;
-                toggleVideoButton.textContent = 'Stop Video';
-            }
-        }
-
-        const toggleAudioButton = document.getElementById('toggle-audio-button');
-
-        toggleAudioButton.addEventListener('click', toggleAudio);
-
-        function toggleAudio() {
-            const audioTrack = stream.getAudioTracks()[0];
-
-            if (audioTrack.enabled) {
-                audioTrack.enabled = false;
-                toggleAudioButton.textContent = 'Unmute';
-            } else {
-                audioTrack.enabled = true;
-                toggleAudioButton.textContent = 'Mute';
-            }
-        }
-    }).catch((err) => {
-        console.log(err);
-    });
-
-
-    socket.on('user-disconnected', userId => {
-        if (peers[userId]) peers[userId].close()
-    })
-
-    // when user joins 
-    myPeer.on('open', id => {
-        socket.emit('join-room', room, id) // inturn calls user-connected
-    })
-
-    function connectToNewUser(userId, stream) {
-        const call = myPeer.call(userId, stream)           // sends our video to others
+    // listens or receives other user video to render on our screen
+    myPeer.on('call', call => {
+        call.answer(stream)                             // it is like answering video call
         const video = document.createElement('video')
-        call.on('stream', userVideoStream => {            // they sends their video 
-            addVideoStream(video, userVideoStream)        // inturn calls this function to render
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)      // and send back video to them
         })
-        call.on('close', () => {                         // when other left, video will be removed
-            video.remove()
-        })
+    })
 
+    // sends video to other user
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
 
-        peers[userId] = call
+    const toggleVideoButton = document.getElementById('toggle-video-button');
+    toggleVideoButton.addEventListener('click', toggleVideoStream);
+
+    function toggleVideoStream() {
+        const videoTrack = stream.getVideoTracks()[0];
+
+        if (videoTrack.enabled) {
+            videoTrack.enabled = false;
+            myVideo.srcObject.getVideoTracks()[0].enabled = false;
+            toggleVideoButton.textContent = 'Start Video';
+        } else {
+            videoTrack.enabled = true;
+            myVideo.srcObject.getVideoTracks()[0].enabled = true;
+            toggleVideoButton.textContent = 'Stop Video';
+        }
     }
 
-    function addVideoStream(video, stream) {
-        video.srcObject = stream
-        video.addEventListener('loadedmetadata', () => {
-            video.play()
-        })
-        videoGrid.append(video)
+    const toggleAudioButton = document.getElementById('toggle-audio-button');
+
+    toggleAudioButton.addEventListener('click', toggleAudio);
+
+    function toggleAudio() {
+        const audioTrack = stream.getAudioTracks()[0];
+
+        if (audioTrack.enabled) {
+            audioTrack.enabled = false;
+            toggleAudioButton.textContent = 'Unmute';
+        } else {
+            audioTrack.enabled = true;
+            toggleAudioButton.textContent = 'Mute';
+        }
     }
+}).catch((err) => {
+    console.log(err);
+});
+
+// remove user from peer object
+socket.on('user-disconnected', userId => {
+    if (peers[userId]) peers[userId].close()
+})
+
+// when user joins 
+myPeer.on('open', id => {
+    socket.emit('join-room', room, id) // inturn calls user-connected
+})
+
+function connectToNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream)           // sends our video to others
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {            // they sends their video 
+        addVideoStream(video, userVideoStream)        // inturn calls this function to render
+    })
+    call.on('close', () => {                         // when other left, video will be removed
+        video.remove()
+    })
+
+
+    peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
 
